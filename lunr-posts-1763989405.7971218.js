5f6c634d13fb05441d831e130addfc31265bbf32
band <band@users.noreply.github.com>
2026-06-22 14:04:16 +0000

    Deploying to gh-pages from @ band/fedisLab@c8f37b0f03e9ac154330d456f01dd627fab06cf1 🚀

--- lunr-posts-1763989405.7971218.js
+++
@@ -1,1 +0,0 @@
-lunr_posts= [{'link': '/2024-08-23_notes.html', 'title': '2024-08-23 notes'}, {'link': '/README.html', 'title': 'README'}, {'link': '/Misc_resources.html', 'title': 'Misc resources'}]
